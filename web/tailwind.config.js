527a3bebcc08957f06e65ac556f5a94d79513e26
Florian Hoss <mail@florianhoss.de>
2023-10-02 22:35:11 +0200

    Init coder environment

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./web/templates/**/*.html"],
+  content: ["./templates/**/*.html"],
   theme: {
     container: {
       center: true
