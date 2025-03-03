057f240712a57553b62fc2f8dd0c21fdf08cd4d3
Florian Hoss <mail@florianhoss.de>
2025-03-03 22:37:17 +0100

    Remove daisyui for upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,21 +8,5 @@ module.exports = {
       center: true
     },
   },
-  plugins: [require('daisyui'), addDynamicIconSelectors()],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          ...require("daisyui/src/theming/themes")["garden"],
-          primary: "#f28c18",
-          secondary: "rgba(70,70,70,0.7)",
-        },
-        dark: {
-          ...require("daisyui/src/theming/themes")["halloween"],
-          secondary: "#a0a0a0",
-        },
-      },
-    ],
-    darkTheme: "dark",
-  },
+  plugins: [addDynamicIconSelectors()],
 };
