e12fe7355f49f31949d79a7f2f4f912028debf59
Florian Hoss <mail@florianhoss.de>
2026-07-22 09:10:01 +0200

    [fix] Fix template panics and align SSE client with SSR HTML

--- assets/js/sse.js
+++ assets/js/sse.js
@@ -19,6 +19,14 @@
     }
   }
 
+  function handleSystemValue(metric, data) {
+    try { setText('value-' + metric, JSON.parse(data)); } catch (e) {}
+  }
+
+  function handleSystemPercentage(metric, data) {
+    try { setWidth('bar-' + metric, JSON.parse(data)); } catch (e) {}
+  }
+
   function updateHourCard(i, h) {
     setText('hour-time-' + i, h.time);
     setIconClass('hour-icon-' + i, h.icon);
@@ -33,12 +41,14 @@
       var h = hours[i];
       var card = document.createElement('div');
       var responsive = i >= 6 ? 'hidden 2xl:flex' : i >= 4 ? 'hidden xl:flex' : i >= 2 ? 'hidden lg:flex' : '';
-      card.className = 'flex w-16 shrink-0 flex-col items-center gap-1 px-2 py-1.5 text-center ' + responsive;
+      card.className = 'flex w-16 shrink-0 flex-col items-end gap-1 px-2 py-1.5 ' + responsive;
       card.innerHTML =
-        '<div id="hour-time-' + i + '" class="text-secondary text-xs">' + h.time + '</div>' +
-        '<div id="hour-icon-' + i + '" class="icon size-7 shrink-0 ' + h.icon + '"></div>' +
-        '<div id="hour-temp-' + i + '" class="font-semibold text-sm">' + h.temperature + '</div>' +
-        '<div class="flex flex-col gap-0.5 text-secondary text-xs w-full">' +
+        '<div class="flex flex-col items-end gap-0.5">' +
+          '<div id="hour-time-' + i + '" class="text-secondary text-xs">' + h.time + '</div>' +
+          '<div id="hour-icon-' + i + '" class="icon size-8 shrink-0 ' + h.icon + '"></div>' +
+          '<div id="hour-temp-' + i + '" class="font-semibold text-sm">' + h.temperature + '</div>' +
+        '</div>' +
+        '<div class="flex flex-col items-end gap-0.5 text-secondary text-xs w-full">' +
           '<div class="flex items-center justify-center gap-1 whitespace-nowrap">' +
             '<span class="icon-[carbon--windy] size-3.5 shrink-0"></span>' +
             '<div id="hour-wind-' + i + '" class="text-secondary whitespace-nowrap">' + (h.wind_speed || '') + '</div>' +
@@ -52,76 +62,53 @@
     }
   }
 
-  function handleSystem(name, data) {
-    var d = JSON.parse(data);
-    setText('value-' + name, d.value);
-    setWidth('bar-' + name, d.percentage);
-  }
-
   function handleWeather(name, data) {
-    if (name === 'current') {
-      var d = JSON.parse(data);
-      setIconClass('weather-icon-0', d.icon);
-      setText('day-name-0', d.name);
-      setText('temp', d.more.current_temperature);
-      setText('max-temp-0', d.temperature_max);
-      setText('min-temp-0', d.temperature_min);
-      setText('humidity', d.more.humidity);
-      setText('wind', (d.more.wind_speed || ''));
-      setText('sunrise', d.more.sunrise);
-      setText('apparent', d.more.apparent_temperature);
-      setText('sunset', d.more.sunset);
-      return;
-    }
-    if (name === 'hourly') {
-      var hours = JSON.parse(data);
-      var container = document.getElementById('hourly');
-      if (!container) return;
-      var existing = container.children;
-      if (existing.length !== hours.length) {
-        rebuildHourly(container, hours);
-      } else {
-        for (var i = 0; i < hours.length; i++) {
-          updateHourCard(i, hours[i]);
+    try {
+      if (name === 'current') {
+        var d = JSON.parse(data);
+        setIconClass('weather-icon-0', d.icon);
+        setText('day-name-0', d.name);
+        setText('temp', d.more.current_temperature);
+        setText('max-temp-0', d.temperature_max);
+        setText('min-temp-0', d.temperature_min);
+        setText('humidity', d.more.humidity);
+        setText('wind', (d.more.wind_speed || ''));
+        setText('sunrise', d.more.sunrise);
+        setText('apparent', d.more.apparent_temperature);
+        setText('sunset', d.more.sunset);
+        return;
+      }
+      if (name === 'hourly') {
+        var hours = JSON.parse(data);
+        var container = document.getElementById('hourly');
+        if (!container) return;
+        var existing = container.children;
+        if (existing.length !== hours.length) {
+          rebuildHourly(container, hours);
+        } else {
+          for (var i = 0; i < hours.length; i++) {
+            updateHourCard(i, hours[i]);
+          }
         }
       }
-      return;
-    }
-    if (name === 'forecast') {
-      var days = JSON.parse(data);
-      for (var i = 1; i < days.length; i++) {
-        var d = days[i];
-        setIconClass('weather-icon-' + i, d.icon);
-        setText('day-name-' + i, d.name);
-        setText('max-temp-' + i, d.temperature_max);
-        setText('min-temp-' + i, d.temperature_min);
-      }
-    }
+    } catch (e) {}
   }
 
-  var handlers = { system: handleSystem, weather: handleWeather };
-
-  function connect(url, stream) {
+  function connect(url) {
     var es = new EventSource(url);
-    es.onmessage = function (e) {
-      var handler = handlers[stream];
-      if (handler) {
-        try { handler('message', e.data); } catch (err) {}
-      }
-    };
-    es.addEventListener('cpu', function (e) { handleSystem('cpu', e.data); });
-    es.addEventListener('ram', function (e) { handleSystem('ram', e.data); });
-    es.addEventListener('disk', function (e) { handleSystem('disk', e.data); });
+    es.addEventListener('cpu-value', function (e) { handleSystemValue('cpu', e.data); });
+    es.addEventListener('cpu-percentage', function (e) { handleSystemPercentage('cpu', e.data); });
+    es.addEventListener('ram-value', function (e) { handleSystemValue('ram', e.data); });
+    es.addEventListener('ram-percentage', function (e) { handleSystemPercentage('ram', e.data); });
+    es.addEventListener('disk-value', function (e) { handleSystemValue('disk', e.data); });
+    es.addEventListener('disk-percentage', function (e) { handleSystemPercentage('disk', e.data); });
     es.addEventListener('current', function (e) { handleWeather('current', e.data); });
-    es.addEventListener('forecast', function (e) { handleWeather('forecast', e.data); });
     es.addEventListener('hourly', function (e) { handleWeather('hourly', e.data); });
   }
 
   function init() {
     document.querySelectorAll('[data-sse]').forEach(function (r) {
-      var url = r.getAttribute('data-sse');
-      var stream = url.split('stream=')[1];
-      connect(url, stream);
+      connect(r.getAttribute('data-sse'));
     });
   }
 
